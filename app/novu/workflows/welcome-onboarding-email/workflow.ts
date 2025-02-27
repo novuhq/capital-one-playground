c946789956fb28afe1b419259f032937ba08ce2d
Dima Grossman <dima@grossman.io>
2025-02-27 10:03:19 +0200

    feat: add base workflows

--- app/novu/workflows/welcome-onboarding-email/workflow.ts
+++ app/novu/workflows/welcome-onboarding-email/workflow.ts
@@ -1,13 +1,31 @@
-import { workflow } from "@novu/framework";
-import { renderEmail } from "../../emails/novu-onboarding-email";
-import { emailControlSchema, payloadSchema } from "./schemas";
+import { workflow } from '@novu/framework';
+import { z } from 'zod';
+import { renderEmail } from '../../emails/novu-onboarding-email';
+import { emailControlSchema, payloadSchema } from './schemas';
 
 export const welcomeOnboardingEmail = workflow(
-  "welcome-onboarding-email",
+  'onboarding-workflow',
   async ({ step, payload }) => {
+    const customResolver = await step.custom(
+      'custom-resolver',
+      async () => {
+        const shouldSkip = await mockInspectionService(300);
+
+        return {
+          shouldSkip,
+        };
+      },
+      {
+        outputSchema: z.object({
+          shouldSkip: z.boolean(),
+        }),
+      }
+    );
+
     await step.email(
-      "send-email",
-      async (controls) => {
+      'send-email',
+      async controls => {
+        console.log({ controls });
         return {
           subject: controls.subject,
           body: renderEmail(controls, payload),
@@ -15,18 +33,58 @@ export const welcomeOnboardingEmail = workflow(
       },
       {
         controlSchema: emailControlSchema,
-      },
+        skip: async () => {
+          const shouldContinue = await mockInspectionService(300);
+
+          return !shouldContinue;
+        },
+      }
     );
 
-    await step.inApp("In-App Step", async () => {
+    await step.inApp('In-App Step', async () => {
       return {
         subject: payload.inAppSubject,
         body: payload.inAppBody,
         avatar: payload.inAppAvatar,
       };
     });
+
+    await step.chat(
+      'chat-step',
+      async () => {
+        return {
+          body: 'Base Chat Step Content',
+        };
+      },
+      {
+        providers: {
+          slack: async step => {
+            return {
+              body: 'Hello, world!',
+              blocks: [
+                {
+                  type: 'section',
+                  text: {
+                    type: 'mrkdwn',
+                    text: 'Hello, world!',
+                  },
+                },
+              ],
+            };
+          },
+        },
+      }
+    );
   },
   {
     payloadSchema,
-  },
+  }
 );
+
+function mockInspectionService(seconds: number): Promise<boolean> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, seconds * 1000);
+  });
+}
